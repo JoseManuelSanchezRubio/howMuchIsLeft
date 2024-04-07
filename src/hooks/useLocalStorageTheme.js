82c93d80c1febcfadcbe24f32a6a90ff2429f666
JoseManuelSanchezRubio <josemanuelsanchezrubio@gmail.com>
2024-04-07 20:36:30 +0200

    fix theme stored

--- src/hooks/useLocalStorageTheme.js
+++ src/hooks/useLocalStorageTheme.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { THEME_KEY } from "../constants";
 
 export const useLocalStorageTheme = () => {
-    const themeStored = localStorage.getItem(THEME_KEY);
+    const themeStored = localStorage.getItem(THEME_KEY) || '#212529';
 
     const setLocalStorageTheme = (theme) => {
         localStorage.setItem(THEME_KEY, theme);
